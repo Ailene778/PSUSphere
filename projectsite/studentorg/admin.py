a916fe068fbd55a9b31602230d1b80c947d3d775
Ailene778 <201980327@psu.palawan.edu.ph>
2024-04-11 16:28:13 +0800

    refactor admin.py

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -1,11 +1,26 @@
 from django.contrib import admin
+from .models import College, Program, Organization, Student, OrgMember
 
-# Register your models here.
-from .models import College,Program, Organization, Student , OrgMember
-
-admin.site.register( College )
-admin.site.register( Program )
+admin.site.register(College)
+admin.site.register(Program)
 admin.site.register(Organization)
-admin.site.register(Student)
-admin.site.register(OrgMember)
 
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("student_id", "lastname", "firstname", "middlename", "program")
+    search_fields = ("lastname", "firstname",)
+
+
+@admin.register(OrgMember)
+class OrgMemberAdmin(admin.ModelAdmin):
+    list_display = ("student", "get_member_program", "organization", "date_joined")
+    search_fields = ("student__lastname", "student__firstname")
+
+    def get_member_program(self, obj):
+        return obj.student.program  # Access the program through obj.student
+
+    get_member_program.short_description = "Program"  # Set a custom column header for the program
+
+
+# Note: Make sure to update the search_fields to use double underscores for related fields like student__lastname and student__firstname.
