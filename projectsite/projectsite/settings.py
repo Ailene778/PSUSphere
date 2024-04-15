beed7abe1885756e306ff8c77ee2a3de8e0788e7
Ailene778 <201980327@psu.palawan.edu.ph>
2024-04-15 14:11:39 +0800

    change

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -58,8 +58,8 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         "DIRS": [os.path.join(BASE_DIR,'templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
+        "APP_DIRS": True,
+        "OPTIONS": {
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
