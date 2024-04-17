5262e42b592f7ec09a287a642837f639ef1f676c
Ailene778 <201980327@psu.palawan.edu.ph>
2024-04-17 20:31:58 +0800

    changes

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
-from studentorg.views import HomePageView, OrganizationListView  # Import the OrganizationListView
+from django.urls import path
+from studentorg.views import HomePageView, OrganizationList  # Import OrganizationList
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', HomePageView.as_view(), name='home'),  # URL for the home page
-    path('organizations/', OrganizationListView.as_view(), name='organization-list'),  # URL for organization list
-    # You can add other URL patterns as needed
+    path('', HomePageView.as_view(), name='home'),
+    path('organizations/', OrganizationList.as_view(), name='organization-list'),  # Use OrganizationList
+    # Other URL patterns
 ]
