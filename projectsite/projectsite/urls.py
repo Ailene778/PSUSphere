fc9e28fb5eb56131e2c23ac336560b0b9e061948
Ailene778 <201980327@psu.palawan.edu.ph>
2024-04-18 15:22:32 +0800

    change

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView
-OrganizationUpdateViews
+from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView  # Import OrganizationUpdateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
-    path('organization_list', OrganizationList.as_view(), name='organization-list'),  
-    path('organization_list/<pk>',OrganizationUpdateView.as_view(), name='organization-update'),
-
+    path('organization_list/add/', OrganizationCreateView.as_view(), name='organization-add'),  
+    path('organization_list/<int:pk>/', OrganizationUpdateView.as_view(), name='organization-update'),  # Corrected path
 ]
