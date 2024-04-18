7a2bfdac88aa5a371432d720cf95e0351b645a7e
Ailene778 <201980327@psu.palawan.edu.ph>
2024-04-18 15:48:20 +0800

    change

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView, UpdateView , DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization
 from studentorg.forms import OrganizationForm
 from django.urls import reverse_lazy
@@ -11,17 +11,18 @@ class HomePageView(ListView):
     template_name = "home.html"
 
 class OrganizationList(ListView):
-    model= Organizationcontext_object_name = 'Organization'
+    model = Organization
+    context_object_name = 'organization'  # Corrected context_object_name
     template_name = 'org_list.html'
-    paginate_by =5
+    paginate_by = 5
 
 class OrganizationCreateView(CreateView):
-    model=Organization
-    form_class=OrganizationForm
-    template_name='org_add.html'
-    success_url=reverse_lazy('organization-list')
+    model = Organization
+    form_class = OrganizationForm
+    template_name = 'org_add.html'
+    success_url = reverse_lazy('organization-list')  # Corrected success_url
 
 class OrganizationDeleteView(DeleteView):
-    model=Organization
-    template_name='org_del.html'
-    success_url=reverse_lazy('organization_list')
+    model = Organization
+    template_name = 'org_del.html'
+    success_url = reverse_lazy('organization-list')  # Corrected success_url
