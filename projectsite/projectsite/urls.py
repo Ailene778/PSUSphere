0b2051d56192895303dd3d27fa86664fd3b83087
Ailene778 <201980327@psu.palawan.edu.ph>
2024-04-18 17:28:02 +0800

    changes

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
 from django.urls import path
-from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView
-from studentorg import views
+from studentorg.views import (
+    HomePageView,
+    OrganizationCreateView,
+    OrganizationListView,
+    OrganizationUpdateView,
+    OrganizationDeleteView  # Added import for OrganizationDeleteView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', HomePageView.as_view(), name='home'),
-    path('organization_list', OrganizationListView.as_view(), name='organization-list'),
+    path('organization_list/', OrganizationListView.as_view(), name='organization-list'),  # Added trailing slash
     path('organization_list/add/', OrganizationCreateView.as_view(), name='organization-add'),  
     path('organization_list/<int:pk>/', OrganizationUpdateView.as_view(), name='organization-update'),
+    path('organization_list/<int:pk>/delete/', OrganizationDeleteView.as_view(), name='organization-delete'),  # Added trailing slash and name
 ]
