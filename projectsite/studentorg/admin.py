e15e1bb08321f786095f0e6176142a7f3444e69d
Ailene778 <201980327@psu.palawan.edu.ph>
2024-04-23 11:29:55 +0800

    change

--- projectsite/studentorg/admin.py
+++ projectsite/studentorg/admin.py
@@ -5,22 +5,19 @@ admin.site.register(College)
 admin.site.register(Program)
 admin.site.register(Organization)
 
-
 @admin.register(Student)
-class StudentAdmin(admin.ModelAdmin):
+class StudentAdmin (admin.ModelAdmin):
     list_display = ("student_id", "lastname", "firstname", "middlename", "program")
     search_fields = ("lastname", "firstname",)
-
-
 @admin.register(OrgMember)
-class OrgMemberAdmin(admin.ModelAdmin):
-    list_display = ("student", "get_member_program", "organization", "date_joined")
-    search_fields = ("student__lastname", "student__firstname")
+class OrgMemberAdmin (admin.ModelAdmin):
+    list_display = ("student", "get_member_program", "organization", "date_joined",)
+    search_fields = ("student_lastname", "student_firstname",)
 
     def get_member_program(self, obj):
-        return obj.student.program  # Access the program through obj.student
+        try:
+            member = Student.objects.get(id=obj.student_id)
+            return member.program
+        except Student.DoesNotExist:
+            return None
 
-    get_member_program.short_description = "Program"  # Set a custom column header for the program
-
-
-# Note: Make sure to update the search_fields to use double underscores for related fields like student__lastname and student__firstname.
